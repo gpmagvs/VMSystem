b2006a5736d3e133c097ede166b5bbcc11024dcf
jinweihuang20 <jinweihuang1020@gmail.com>
2024-06-11 11:42:28 +0800

    update serilog settings/ update launch.Setting.json

---
+++ ApiLoggingMiddleware.cs
@@ -0,0 +1,88 @@
+﻿using System.Text;
+
+namespace VMSystem
+{
+    public class ApiLoggingMiddleware
+    {
+        private readonly ILogger<ApiLoggingMiddleware> _logger;
+
+        private readonly RequestDelegate _next;
+
+        public ApiLoggingMiddleware(ILogger<ApiLoggingMiddleware> logger, RequestDelegate next)
+        {
+            _logger = logger;
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            // 攔截請求
+            var request = await FormatRequest(context.Request);
+
+            // 紀錄回應前的狀態
+            var originalBodyStream = context.Response.Body;
+
+            using var responseBody = new MemoryStream();
+            context.Response.Body = responseBody;
+
+            await _next(context);
+
+            // 攔截回應
+            var response = await FormatResponse(context.Response);
+
+            // 紀錄資訊
+            _logger.LogInformation("Request: \n{Request}", request);
+            _logger.LogInformation("Response: \n{Response}", response);
+
+
+            // 將原始回應內容寫回
+            await responseBody.CopyToAsync(originalBodyStream);
+        }
+
+        /// <summary>
+        /// 格式化請求
+        /// </summary>
+        private async Task<string> FormatRequest(HttpRequest request)
+        {
+            request.EnableBuffering();
+            var headers = FormatHeaders(request.Headers);
+            var body = await new StreamReader(request.Body, Encoding.UTF8).ReadToEndAsync();
+            request.Body.Position = 0;
+            var ip = request.HttpContext.Connection.RemoteIpAddress?.ToString();
+
+            return $"Method: {request.Method}\n" +
+                   $"URL: {request.Scheme}://{request.Host}{request.Path}{request.QueryString}\n" +
+                   $"Body: {body}\n" +
+                   $"IP: {ip}";
+        }
+
+        /// <summary>
+        /// 格式化回應
+        /// </summary>
+        private async Task<string> FormatResponse(HttpResponse response)
+        {
+            response.Body.Seek(0, SeekOrigin.Begin);
+            var headers = FormatHeaders(response.Headers);
+            var body = await new StreamReader(response.Body, Encoding.UTF8).ReadToEndAsync();
+            response.Body.Seek(0, SeekOrigin.Begin);
+
+            return $"Status code: {response.StatusCode}\n" +
+                   $"Headers: \n{headers}" +
+                   $"Body: {body}";
+        }
+
+        /// <summary>
+        /// 格式化標頭
+        /// </summary>
+        private string FormatHeaders(IHeaderDictionary headers)
+        {
+            var formattedHeaders = new StringBuilder();
+            foreach (var (key, value) in headers)
+            {
+                formattedHeaders.AppendLine($"\t{key}: {string.Join(",", value)}");
+            }
+
+            return formattedHeaders.ToString();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Http.Json;
 using Microsoft.AspNetCore.WebSockets;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.FileProviders;
+using Serilog;
+using Serilog.Filters;
 using VMSystem;
 using VMSystem.BackgroundServices;
 using VMSystem.Controllers;
@@ -15,6 +17,38 @@ using VMSystem.Services;
 Startup.ConfigurationInit();
 
 var builder = WebApplication.CreateBuilder(args);
+
+string logRootFolder = AGVSConfigulator.SysConfigs.LogFolder;
+builder.Host.UseSerilog((context, services, configuration) => configuration
+    .ReadFrom.Services(services)
+    .Enrich.FromLogContext()
+    //全部的LOG但不包含EF Core Log
+    .WriteTo.Logger(lc => lc
+                .WriteTo.Console()
+                .Filter.ByExcluding(Matching.FromSource("Microsoft.EntityFrameworkCore")) // 過濾EF Core Log  
+                .WriteTo.File(
+                    path: $"{logRootFolder}/VMSystem/log-.log", // 路徑
+                    rollingInterval: RollingInterval.Day, // 每小時一個檔案
+                    retainedFileCountLimit: 24 * 90,// 最多保留 30 天份的 Log 檔案
+                    outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff} [{Level:u3}] {Message:lj}{NewLine}{Exception}",
+                    rollOnFileSizeLimit: false,
+                    fileSizeLimitBytes: null
+                    ))
+////只有 AGVSystem.ApiLoggingMiddleware 
+//.WriteTo.Logger(lc => lc
+//                .WriteTo.Console()
+//                .Filter.ByIncludingOnly(Matching.FromSource("AGVSystem.ApiLoggingMiddleware"))
+//                .WriteTo.File(
+//                    path: $"{logRootFolder}/AGVS/api/log-.log",
+//                    rollingInterval: RollingInterval.Day,
+//                    retainedFileCountLimit: 24 * 90,
+//                    outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff} [{Level:u3}] {Message:lj}{NewLine}{Exception}",
+//                    rollOnFileSizeLimit: false,
+//                    fileSizeLimitBytes: null
+//                )
+//            )
+);
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -72,6 +106,7 @@ catch (Exception ex)
     Thread.Sleep(3000);
     Environment.Exit(1);
 }
+app.UseMiddleware<ApiLoggingMiddleware>();
 app.UseSwagger();
 app.UseSwaggerUI();
 app.UseDefaultFiles(new DefaultFilesOptions());
