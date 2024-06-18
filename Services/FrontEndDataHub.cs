8aa16f7d418663030534d5c8eba65e48786041a7
jinweihuang20 <ss610349@gmaol.com>
2024-06-18 12:03:17 +0800

    新增 signalR client side 訊息接收處理

--- Services/FrontEndDataHub.cs
+++ Services/FrontEndDataHub.cs
@@ -10,6 +10,16 @@ namespace VMSystem.Services
         {
             this.logger = logger;
         }
+
+
+        public async Task SendMessage(string user, string message)
+        {
+            if (message == "fetch-data")
+            {
+                await Clients.All.SendAsync("ReceiveData", "VMS", FrontEndDataCollectionBackgroundService._previousData);
+            }
+            //return Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
         public override async Task OnConnectedAsync()
         {
             logger.LogTrace($"{this.Context.ConnectionId} Connected");
