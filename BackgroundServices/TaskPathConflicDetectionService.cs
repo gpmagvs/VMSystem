33dacff3998e9c08d1edbab0236b63e163f8578a
jinweihuang20 <jinweihuang1020@gmail.com>
2024-08-20 13:35:27 +0800

    背景監視AGV路徑干涉與碰撞預估改為參數為true時採取行動，否則僅記錄LOG

--- BackgroundServices/TaskPathConflicDetectionService.cs
+++ BackgroundServices/TaskPathConflicDetectionService.cs
@@ -25,15 +25,11 @@ namespace VMSystem.BackgroundServices
                 {
                     await Task.Delay(1);
 
-                    if (!TrafficControlCenter.TrafficControlParameters.Navigation.PathConflicBackgroundMonitor)
-                        continue;
-
                     //監視每一台車當前的軌跡，若有軌跡衝突則進行處理
                     foreach (var vehicle in VMSManager.AllAGV)
                     {
                         if (vehicle.main_state != AGVSystemCommonNet6.clsEnums.MAIN_STATUS.RUN)
                             continue;
-
                         await _TryGetConflic(vehicle);
                     }
 
@@ -57,16 +53,23 @@ namespace VMSystem.BackgroundServices
                             if (conflicVehicles_BodyCollision.Any(v => vehicle.states.Coordination.CalculateDistance(v.states.Coordination) < 2))
                             {
                                 _IsEmergencyStop = true;
-                                vehicle.main_state = AGVSystemCommonNet6.clsEnums.MAIN_STATUS.DOWN;
-                                AlarmManagerCenter.AddAlarmAsync(ALARMS.Path_Conflic_But_Dispatched, Equipment_Name: vehicle.Name, location: vehicle.currentMapPoint.Graph.Display, taskName: vehicle.CurrentRunningTask().OrderData.TaskName, level: ALARM_LEVEL.ALARM);
-                                vehicle.TaskExecuter.EmergencyStop();
+
+                                if (TrafficControlCenter.TrafficControlParameters.Navigation.PathConflicBackgroundMonitor)
+                                {
+                                    vehicle.main_state = AGVSystemCommonNet6.clsEnums.MAIN_STATUS.DOWN;
+                                    AlarmManagerCenter.AddAlarmAsync(ALARMS.Path_Conflic_But_Dispatched, Equipment_Name: vehicle.Name, location: vehicle.currentMapPoint.Graph.Display, taskName: vehicle.CurrentRunningTask().OrderData.TaskName, level: ALARM_LEVEL.ALARM);
+                                    vehicle.TaskExecuter.EmergencyStop();
+                                }
                                 await Task.Delay(1000);
                             }
                             else
                             {
                                 _IsEmergencyStop = false;
-                                AlarmManagerCenter.AddAlarmAsync(ALARMS.Path_Conflic_But_Dispatched, Equipment_Name: vehicle.Name, location: vehicle.currentMapPoint.Graph.Display, taskName: vehicle.CurrentRunningTask().OrderData.TaskName, level: ALARM_LEVEL.WARNING);
-                                vehicle.CurrentRunningTask().CycleStopRequestAsync();
+                                if (TrafficControlCenter.TrafficControlParameters.Navigation.PathConflicBackgroundMonitor)
+                                {
+                                    AlarmManagerCenter.AddAlarmAsync(ALARMS.Path_Conflic_But_Dispatched, Equipment_Name: vehicle.Name, location: vehicle.currentMapPoint.Graph.Display, taskName: vehicle.CurrentRunningTask().OrderData.TaskName, level: ALARM_LEVEL.WARNING);
+                                    vehicle.CurrentRunningTask().CycleStopRequestAsync();
+                                }
                                 await Task.Delay(1000);
                             }
                             LogVehicleStatus(vehicle, conflicVehicles_BodyCollision, _IsEmergencyStop);
@@ -77,8 +80,13 @@ namespace VMSystem.BackgroundServices
                             if (vehicle.CurrentRunningTask().ActionType == AGVSystemCommonNet6.AGVDispatch.Messages.ACTION_TYPE.None)
                             {
                                 logger.LogCritical($"Vehicle {vehicle.Name} has path conflic with {string.Join(",", conflicVehicles.Select(agv => agv.Name))}");
-                                AlarmManagerCenter.AddAlarmAsync(ALARMS.VEHICLES_TRAJECTORY_CONFLIC, level: ALARM_LEVEL.WARNING);
-                                vehicle.CurrentRunningTask().CycleStopRequestAsync();
+
+                                if (TrafficControlCenter.TrafficControlParameters.Navigation.PathConflicBackgroundMonitor)
+                                {
+                                    AlarmManagerCenter.AddAlarmAsync(ALARMS.VEHICLES_TRAJECTORY_CONFLIC, level: ALARM_LEVEL.WARNING);
+                                    vehicle.CurrentRunningTask().CycleStopRequestAsync();
+                                }
+
                             }
                             //vehicle.TaskExecuter.EmergencyStop();
                             //foreach (var conflicVehicle in conflicVehicles)
