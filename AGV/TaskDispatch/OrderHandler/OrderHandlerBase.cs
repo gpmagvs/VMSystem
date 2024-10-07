57bc9d9e3743a71c3be5bc2818810847d69104f1
jinweihuang20 <jinweihuang1020@gmail.com>
2024-10-07 12:10:39 +0800

    修正clsAlarmDto Description屬性回傳為中文在前、英文在括號內

--- AGV/TaskDispatch/OrderHandler/OrderHandlerBase.cs
+++ AGV/TaskDispatch/OrderHandler/OrderHandlerBase.cs
@@ -374,7 +374,7 @@ namespace VMSystem.AGV.TaskDispatch.OrderHandler
                 UnRegistPoints();
                 OrderData.State = TASK_RUN_STATUS.FAILURE;
                 OrderData.FinishTime = DateTime.Now;
-                OrderData.FailureReason = alarmDto.AlarmCode == 0 ? FailReason : $"[{alarmDto.AlarmCode}] {alarmDto.Description_Zh}({alarmDto.Description_En})";
+                OrderData.FailureReason = alarmDto.AlarmCode == 0 ? FailReason : $"[{alarmDto.AlarmCode}] {alarmDto.Description})";
 
                 if (Agv != null && (alarm == ALARMS.AGV_STATUS_DOWN || Agv?.main_state == MAIN_STATUS.DOWN))
                 {
