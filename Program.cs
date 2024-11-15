e1e661cc03430f6324f24ea9622c9e68701ea95f
haochang <haochang0926@gmail.com>
2024-11-15 09:00:26 +0800

    Code clean : Remove useless code line.

--- Program.cs
+++ Program.cs
@@ -16,7 +16,6 @@ using VMSystem;
 using VMSystem.BackgroundServices;
 using VMSystem.Services;
 
-Console.WriteLine("Hello world!");
 if (ProcessTools.IsProcessRunning("VMSystem", out List<int> pids))
 {
     Console.WriteLine($"VMS Program is already running({string.Join(",", pids)})");
