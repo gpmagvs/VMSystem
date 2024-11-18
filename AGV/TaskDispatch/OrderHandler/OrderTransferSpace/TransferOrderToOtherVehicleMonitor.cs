8fce6d321adcfe65a822983742a537a146dad74b
jinweihuang20 <jinweihuang1020@gmail.com>
2024-11-18 11:30:55 +0800

    Optimized : [TransferOrderToOtherVehicleMonitor] 評估車輛與目的地工作站的距離使用最短路徑的走行距離並加LOG

--- AGV/TaskDispatch/OrderHandler/OrderTransferSpace/TransferOrderToOtherVehicleMonitor.cs
+++ AGV/TaskDispatch/OrderHandler/OrderTransferSpace/TransferOrderToOtherVehicleMonitor.cs
@@ -28,9 +28,9 @@ namespace VMSystem.AGV.TaskDispatch.OrderHandler.OrderTransferSpace
         {
             betterVehicle = null;
             //評估是否有其他車輛當前位置
-            double distanceToWorkStationOfOwner = TargetWorkStationMapPoint.CalculateDistance(orderOwner.states.Coordination);
+            double distanceToWorkStationOfOwner = GetTravelDistanceToTargetWorkStation(orderOwner);
 
-            var moreNearToGoalVehicles = OtherVehicles.ToDictionary(vehicle => vehicle, vehicle => TargetWorkStationMapPoint.CalculateDistance(vehicle.states.Coordination))
+            var moreNearToGoalVehicles = OtherVehicles.ToDictionary(vehicle => vehicle, vehicle => GetTravelDistanceToTargetWorkStation(vehicle))
                          .OrderBy(kp => kp.Value)
                          .Where(kp => kp.Value < distanceToWorkStationOfOwner)
                          .ToDictionary(kp => kp.Key, kp => kp.Value);
@@ -40,6 +40,13 @@ namespace VMSystem.AGV.TaskDispatch.OrderHandler.OrderTransferSpace
                                                                 .ToList();
             if (idleOrChargingVehicles.Any())
                 betterVehicle = idleOrChargingVehicles.First().Key;
+
+            if (betterVehicle != null)
+            {
+                string _distanceEvealuation = string.Join(",", idleOrChargingVehicles.OrderBy(kp => kp.Key.Name).Select(kp => $"{kp.Key.Name}-{kp.Value}m").ToList());
+                Log($"Better Vehicle Found=>{betterVehicle.Name}.Distances to target workstation of origin order owner:{distanceToWorkStationOfOwner}m|| Distances to target workstation of candicators: {_distanceEvealuation}");
+            }
+
             return betterVehicle != null;
         }
 
@@ -59,6 +66,21 @@ namespace VMSystem.AGV.TaskDispatch.OrderHandler.OrderTransferSpace
         private bool IsVehicleNoOrder(IAGV vehicle)
         {
             return vehicle.taskDispatchModule.OrderExecuteState == clsAGVTaskDisaptchModule.AGV_ORDERABLE_STATUS.NO_ORDER;
+        }
+
+
+        private double GetTravelDistanceToTargetWorkStation(IAGV vehicle)
+        {
+            PathFinder _pathFinder = new PathFinder();
+            AGVSystemCommonNet6.MAP.PathFinder.clsPathInfo _pathInfo = _pathFinder.FindShortestPath(vehicle.currentMapPoint.TagNumber, TargetWorkStationMapPoint.TagNumber, new PathFinder.PathFinderOption
+            {
+                Algorithm = PathFinder.PathFinderOption.ALGORITHM.Dijsktra,
+                Strategy = PathFinder.PathFinderOption.STRATEGY.SHORST_DISTANCE,
+                OnlyNormalPoint = false,
+            });
+            if (_pathInfo == null || _pathInfo.stations.Count == 0)
+                return double.MaxValue;
+            return _pathInfo.total_travel_distance;
         }
 
     }
