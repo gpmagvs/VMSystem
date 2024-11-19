d9396e6011dd38814be5f5af8ebb80da3bce5478
jinweihuang20 <jinweihuang1020@gmail.com>
2024-11-19 11:51:27 +0800

    強型別宣告  : class [TransferOrderToOtherVehicleMonitor ]___ var-> List<KeyValuePair<IAGV, double>>

--- AGV/TaskDispatch/OrderHandler/OrderTransferSpace/TransferOrderToOtherVehicleMonitor.cs
+++ AGV/TaskDispatch/OrderHandler/OrderTransferSpace/TransferOrderToOtherVehicleMonitor.cs
@@ -40,12 +40,12 @@ namespace VMSystem.AGV.TaskDispatch.OrderHandler.OrderTransferSpace
                              .Where(kp => kp.Value < distanceToWorkStationOfOwner)
                              .ToDictionary(kp => kp.Key, kp => kp.Value);
                 //過濾出車上無貨且正在IDLE 或 正在執行充電任務訂單的車輛
-                var idleOrChargingVehicles = moreNearToGoalVehicles.Where(kp => kp.Key.main_state != clsEnums.MAIN_STATUS.DOWN) //不是當機的車輛
-                                                                   .Where(kp => !kp.Key.IsAGVHasCargoOrHasCargoID()) //車上無貨的車輛
-                                                                   .Where(kp => kp.Key.online_state == clsEnums.ONLINE_STATE.ONLINE) //上線中車輛
-                                                                   .Where(kp => kp.Key.batteryStatus > IAGV.BATTERY_STATUS.LOW) //確認電池狀態
-                                                                   .Where(kp => IsVehicleNoOrder(kp.Key) || IsVehicleExecutingChargeTask(kp.Key) || IsVehicleLoading(kp.Key)) //執行充電任務中 or 空閒中車輛 or 在執行放貨任務的車
-                                                                   .ToList();
+                List<KeyValuePair<IAGV, double>> idleOrChargingVehicles = moreNearToGoalVehicles.Where(kp => kp.Key.main_state != clsEnums.MAIN_STATUS.DOWN) //不是當機的車輛
+                                                                                                .Where(kp => !kp.Key.IsAGVHasCargoOrHasCargoID()) //車上無貨的車輛
+                                                                                                .Where(kp => kp.Key.online_state == clsEnums.ONLINE_STATE.ONLINE) //上線中車輛
+                                                                                                .Where(kp => kp.Key.batteryStatus > IAGV.BATTERY_STATUS.LOW) //確認電池狀態
+                                                                                                .Where(kp => IsVehicleNoOrder(kp.Key) || IsVehicleExecutingChargeTask(kp.Key) || IsVehicleLoading(kp.Key)) //執行充電任務中 or 空閒中車輛 or 在執行放貨任務的車
+                                                                                                .ToList();
                 if (idleOrChargingVehicles.Any())
                     betterVehicle = idleOrChargingVehicles.First().Key;
 
