d3cc9ff8405b2e982dd28c8ef8cda45b40b9f869
jinweihuang20 <jinweihuang1020@gmail.com>
2024-11-21 13:26:44 +0800

    Code Refactory : 將 ParkOrderHandler 從 ChargeOrderHandler.cs 中移出

--- AGV/TaskDispatch/OrderHandler/ChargeOrderHandler.cs
+++ AGV/TaskDispatch/OrderHandler/ChargeOrderHandler.cs
@@ -108,14 +108,5 @@ namespace VMSystem.AGV.TaskDispatch.OrderHandler
         }
     }
 
-    public class ParkOrderHandler : ChargeOrderHandler
-    {
-        public ParkOrderHandler() : base() { }
-        public ParkOrderHandler(AGVSDbContext agvsDb, SemaphoreSlim taskTbModifyLock) : base(agvsDb, taskTbModifyLock)
-        {
-        }
-
-        public override ACTION_TYPE OrderAction => ACTION_TYPE.Park;
-    }
 
 }
