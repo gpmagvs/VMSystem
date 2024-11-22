b1f799ef34b2dd93dad8728a50a52c78e9143e85
jinweihuang20 <jinweihuang1020@gmail.com>
2024-11-22 12:43:56 +0800

    新增擴充功能 : VMSMapExtension.IsEntryPointOfWorkStation

--- Extensions/VMSMapExtension.cs
+++ Extensions/VMSMapExtension.cs
@@ -1,10 +1,22 @@
-﻿namespace VMSystem.Extensions
+﻿using AGVSystemCommonNet6.MAP;
+using VMSystem.AGV.TaskDispatch.Tasks;
+
+namespace VMSystem.Extensions
 {
     public static class VMSMapExtension
     {
         public static string GetDisplayAtCurrentMap(this int tagNumber)
         {
             return StaMap.GetPointByTagNumber(tagNumber)?.Graph.Display ?? string.Empty;
+        }
+
+        public static bool IsEntryPointOfWorkStation(this int tagNumber, out IEnumerable<MapPoint> workStations)
+        {
+            workStations = new List<MapPoint>();
+            if (!StaMap.TryGetPointByTagNumber(tagNumber, out MapPoint pt))
+                return false;
+            workStations = pt.TargetWorkSTationsPoints();
+            return workStations.Any();
         }
     }
 }
