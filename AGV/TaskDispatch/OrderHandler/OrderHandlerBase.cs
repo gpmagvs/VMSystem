3374e52fbfc43c0a7a6825ff78d9531c592c3103
jinweihuang20 <101879478+jinweihuang20@users.noreply.github.com>
2024-12-16 23:40:12 +0800

    訂單結束後需解除tag變化事件
    Agv.OnMapPointChanged -=HandleAGVMapPointChanged;

--- AGV/TaskDispatch/OrderHandler/OrderHandlerBase.cs
+++ AGV/TaskDispatch/OrderHandler/OrderHandlerBase.cs
@@ -1,4 +1,4 @@
-﻿using AGVSystemCommonNet6;
+using AGVSystemCommonNet6;
 using AGVSystemCommonNet6.AGVDispatch;
 using AGVSystemCommonNet6.AGVDispatch.Messages;
 using AGVSystemCommonNet6.Alarm;
@@ -212,6 +212,7 @@ namespace VMSystem.AGV.TaskDispatch.OrderHandler
                     //Agv.taskDispatchModule.AsyncTaskQueueFromDatabase();
                     double finalMileageOfVehicle = Agv.states.Odometry;
                     OrderData.TotalMileage = finalMileageOfVehicle - beginMileageOfVehicle;
+                    Agv.OnMapPointChanged -= HandleAGVMapPointChanged;
                     OrderData.TagsTracking = string.Join("->", TagsTracking);
                     ModifyOrder(OrderData);
                     DisposeActionOfCompleteTasks();
