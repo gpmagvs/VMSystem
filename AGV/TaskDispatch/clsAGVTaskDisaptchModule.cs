605d722704e3d85acc5f2ed92f71b5bdfda9738c
jinweihuang20 <jinweihuang1020@gmail.com>
2025-01-19 18:09:41 +0800

    優化運轉模式下當AGV車上有貨無法進行充電停車任務的異常新增

--- AGV/TaskDispatch/clsAGVTaskDisaptchModule.cs
+++ AGV/TaskDispatch/clsAGVTaskDisaptchModule.cs
@@ -145,10 +145,11 @@ namespace VMSystem.AGV
                 await Task.Delay(TimeSpan.FromSeconds(AGVSConfigulator.SysConfigs.AutoModeConfigs.AGVIdleTimeUplimitToExecuteChargeTask));
                 if (agv.IsAGVHasCargoOrHasCargoID() && !agv.currentMapPoint.IsCharge)
                 {
-                    if (_charge_forbid_alarm != null)
-                        AlarmManagerCenter.RemoveAlarm(_charge_forbid_alarm.Result);
+                    bool alarmAddedButNotChecked=DatabaseCaches.Alarms.UnCheckedAlarms.Any(al=>al.Equipment_Name== agv.Name && al.AlarmCode== (int)ALARMS.Cannot_Auto_Parking_When_AGV_Has_Cargo);
+                    if (alarmAddedButNotChecked)
+                        continue;
                     _charge_forbid_alarm = AlarmManagerCenter.AddAlarmAsync(ALARMS.Cannot_Auto_Parking_When_AGV_Has_Cargo, ALARM_SOURCE.AGVS, ALARM_LEVEL.WARNING, agv.Name, agv.currentMapPoint.Graph.Display);
-                    _charge_forbid_alarm.Wait();
+                    await Task.Delay(500);
                     continue;
                 }
 
